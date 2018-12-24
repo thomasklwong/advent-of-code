620b0fa9fd6273a3c2b154675fb90d9ee84f4f98
Thomas Wong <1935201+thomasklwong@users.noreply.github.com>
2018-12-25 07:11:02 +0800

    feat: add puzzle 1 and 2 of day 1

---
+++ 2018/12-01/puzzle1/solve.js
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+const fs = require('fs');
+const readline = require('readline');
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream('input.txt'),
+  crlfDelay: Infinity
+});
+
+let frequency = 0;
+const filter = /([+-])(\d+)/;
+lineReader.on('line', line => {
+  const result = filter.exec(line);
+
+  if (result === null) {
+    console.error(`Cannot find info in line "${line}".`);
+    return;
+  }
+
+  const [_, sign, f] = result;
+
+
+  if (sign === '+') {
+    frequency += parseInt(f);
+  } else if (sign === '-') {
+    frequency -= parseInt(f);
+  } else {
+    console.error(`Unknown operation in line "${line}".`);
+  }
+
+  console.log(`Frequency now: ${frequency}`);
+});
+
+console.warn(`Frequency: ${frequency}`);
---
+++ 2018/12-01/puzzle2/solve.js
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+const fs = require('fs');
+const readline = require('readline');
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream('input.txt'),
+  crlfDelay: Infinity
+});
+
+const extractor = /([+-])(\d+)/;
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+const drifts = [];
+
+let currentFrequency = 0;
+const log = {};
+
+
+lineReader.on('line', line => {
+  const result = extractor.exec(line);
+
+  if (!result) {
+    console.error(`Cannot find info in line "${line}"`);
+    return;
+  }
+
+  const [fullStringThatMatched, sign, driftString] = result;
+  const drift = parseInt(driftString);
+  const func = sign === '+' ? add : sign === '-' ? subtract : void 0;
+
+  if (isNaN(drift)) {
+    console.error(`The drift value, ${driftString}, is not a number.`);
+    return;
+  }
+
+  if (!func) {
+    console.error(`Cannot recognise the sign, ${sign}, of the drift.`);
+    return;
+  }
+
+  drifts.push({
+    func,
+    drift
+  });
+});
+
+function* cycleDrifts() {
+  while (true) {
+    for (const data of drifts) {
+      yield data;
+    }
+  }
+}
+
+const foundTwice = () => {
+  const record = log[currentFrequency];
+
+  if (!record) {
+    log[currentFrequency] = 1;
+    return false;
+  }
+
+  return true;
+};
+
+lineReader.on('close', () => {
+  const iterator = cycleDrifts();
+
+  while (!foundTwice()) {
+    console.log(`Current Frequency: ${currentFrequency}`);
+    console.log(`Haven't landed on same frequency twice yet.`);
+
+    const {
+      value: {
+        func,
+        drift
+      }
+    } = iterator.next();
+
+    currentFrequency = func(currentFrequency, drift);
+  }
+
+  console.log(`Landed on frequency: ${currentFrequency} twice.`);
+});
