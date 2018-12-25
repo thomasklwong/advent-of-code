b19f8d196dd2123e68f4f2c77d809086cbcb91c4
Thomas Wong <1935201+thomasklwong@users.noreply.github.com>
2018-12-25 14:47:35 +0800

    feat: add 12-02-02 answer

---
+++ 2018/12-02/puzzle2/solve.js
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+const reader = require('../../../lib/reader');
+const lineReader = reader.read();
+
+const ids = [];
+
+const mergeAndDiff = (a = '', b = '') => {
+  return a.split('').reduce((acc, char, index) => {
+    if (char === b[index]) {
+      acc.common += char;
+    } else {
+      acc.diff.push(char);
+    }
+
+    return acc;
+  }, {
+    common: '',
+    diff: []
+  });
+};
+
+lineReader.on('line', line => ids.push(line));
+
+lineReader.on('close', () => {
+  let found = false;
+  let result = {};
+
+  ids.sort();
+
+  console.log(ids.length)
+
+  while (!found && ids.length) {
+    const first = ids.shift();
+
+    ids.some(id => {
+      result = mergeAndDiff(id, first);
+      found = result.diff.length === 1;
+
+      console.log(`(${first}, ${id}), common: ${result.common}, diff: ${result.diff}`);
+
+      return found;
+    });
+  }
+
+  console.log(`The common are: ${result.common}`);
+});
